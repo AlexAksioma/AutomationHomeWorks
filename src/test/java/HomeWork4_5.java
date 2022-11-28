902887450868869b16eb2aa1b302be9a10172862
AlexAksioma <alexmedqwerty@gmail.com>
2022-11-28 09:46:01 +0200

    HW5

--- src/test/java/HomeWork4_5.java
+++ src/test/java/HomeWork4_5.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-public class HomeWork4 {
+public class HomeWork4_5 {
     WebDriver wd;
 
     @BeforeMethod
@@ -20,8 +20,10 @@ public class HomeWork4 {
 
     @Test
     public void tableHW(){
-        List<WebElement> elementsTable = wd.findElements(By.cssSelector("#customers tr:nth-child(4) td"));
+        /*List<WebElement> elementsTable = wd.findElements(By.cssSelector("#customers tr:nth-child(4) td"));
+
         System.out.println(elementsTable.size());
+
         for(int i=0; i< elementsTable.size(); i++) {
             WebElement element = elementsTable.get(i);
             System.out.println(element.getText());
@@ -34,17 +36,35 @@ public class HomeWork4 {
             }
         }
         Assert.assertTrue(elementsTable.get(2).getText().equals("Mexico"));
+
+        */
+        // get row 4 content
+        // check if there is text "Mexico" there
+        WebElement element = wd.findElement(By.xpath("//table[@id='customers']//tr[4]"));
+        WebElement element1 = wd.findElement(By.xpath("//*[contains(@class,'ws-table-all')]/tbody/tr[4]"));
+        System.out.println(element.getText());
+        Assert.assertTrue(element.getText().contains("Mexico"));
+
+        Assert.assertEquals(
+                wd.findElement(By.xpath("//table//*[text()='Mexico']")).getText(),"Mexico");
     }
 
     @Test
     public void tableHW2(){
         List<WebElement> elementsTableRow = wd.findElements(By.cssSelector("#customers tr"));
-        int amountTableRows = elementsTableRow.size();
+
+        List<WebElement> elementsTableRow1 = wd.findElements(By.xpath("//table[@id='customers']//tr"));
+        List<WebElement> elementsTableRow2= wd.findElements(By.xpath("//*[@class='ws-table-all']//tr"));
+        int amountTableRows = elementsTableRow2.size();  // print amount of table rows
         System.out.println("amount of table rows = "+amountTableRows);
         System.out.println("===============================");
 
-        List<WebElement> elementsTableCol = wd.findElements(By.cssSelector("#customers tr th"));
-        System.out.println("amount of table cols = "+elementsTableCol.size());
+        List<WebElement> elementsTableCol = wd.findElements(By.cssSelector("#customers th"));
+
+        List<WebElement> elementsTableCol1 = wd.findElements(By.xpath("//*[@id='customers']/tbody//th"));
+        List<WebElement> elementsTableCol2 = wd.findElements(By.xpath("//*[contains(@id,'tomer')]/tbody//th"));
+        List<WebElement> elementsTableCol3 = wd.findElements(By.xpath("//*[starts-with(@id,'cus')]/tbody//th"));
+        System.out.println("amount of table cols = "+elementsTableCol3.size());
         System.out.println("===============================");
 
         List<WebElement> elementsTableContentRow3 = wd.findElements(By.cssSelector("#customers tr:nth-child(3) td"));
@@ -56,27 +76,48 @@ public class HomeWork4 {
             System.out.println(e.getText());
         }
 
-        System.out.println("Now===============================");
+        System.out.println(wd.findElement(By.xpath("//*[contains(@class,'ws-tabl')]//tr[3]")).getText());
+
+        System.out.println(wd.findElement(By.xpath("//table[(@id='customers') and contains(@class,ws-table-all)]//tr[3]")).getText());
+
+        System.out.println(wd.findElement(By.xpath("//table[(@id='customers') and (@class='ws-table-all')]//tr[3]")).getText());
+
+
+        System.out.println("===============================");
         List<WebElement> elementsTableContentLastCol = wd.findElements(By.cssSelector("#customers td:last-child"));
         WebElement elementTh = wd.findElement(By.cssSelector("#customers tr th:last-child"));
         System.out.println(elementTh.getText());
         for (WebElement element: elementsTableContentLastCol) {
             System.out.println(element.getText());
         }
+        System.out.println("-----------------------------------");
         /*for(int i=0; i<elementsTableContentLastCol.size(); i++){
             WebElement content = elementsTableContentLastCol.get(i);
             System.out.println(content.getText());
         }*/
+        // print content of last col
+        List<WebElement> elementLastCol = wd.findElements(By.xpath("//table[@id='customers']//td[3]"));
+        List<WebElement> elementLastCol1 = wd.findElements(By.xpath("//table[@id='customers']//td[last()]"));
+        List<WebElement> elementLastCol2 = wd.findElements(By.xpath("//*[contains(@id,'stomers') and starts-with(@class,'ws')]//td[last()]"));
+        for (WebElement element:elementLastCol2) {
+            System.out.println(element.getText());
+        }
+
 
         System.out.println("===============================");
         List<WebElement> elementsTableContentNames = wd.findElements(By.cssSelector("#customers tr"));
-        for(int i=0; i<elementsTableContentNames.size(); i++) {
-            WebElement elementName = elementsTableContentNames.get(i);
+
+        List<WebElement> elementsTableContentNames1 = wd.findElements(By.xpath("//table[@id='customers']//tr"));
+        List<WebElement> elementsTableContentNames2 = wd.findElements(By.xpath("//*[@class='ws-table-all']/tbody/tr"));
+        for(int i=0; i<elementsTableContentNames2.size(); i++) {
+            WebElement elementName = elementsTableContentNames2.get(i);
             if(elementName.getText().contains("Philip Cramer")){
                 System.out.println("number of row with name Philip Cramer = "+(i+1));
                 return;
             }
         }
+
+
 
     }
 
